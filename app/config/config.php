1c72a63d9ad614f8ac0dc6b0284a797a48e74051
Ihar Petrushenka <petrushen@yahoo.com>
2022-01-26 10:44:54 +0300

    first commit

--- app/config/config.php
+++ app/config/config.php
@@ -2,7 +2,7 @@
 
     define('DB_HOST', 'localhost');  
     define('DB_USER', 'root'); 
-    define('DB_PASS', '80298508852'); 
+    define('DB_PASS', ''); 
     define('DB_NAME', 'products'); 
 
     define('APPROOT', dirname(dirname(__FILE__)));  
